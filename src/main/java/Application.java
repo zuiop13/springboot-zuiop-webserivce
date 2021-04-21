48ea732aefad0e9d973dac5afd56160c1b1787bf
zuiop13 <zuiop13@naver.com>
2021-04-21 15:11:42 +0900

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Application {
+    //psvm
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/springdata";
+        String username = "zuiop13";
+        String password = "pass";
+
+        try(Connection connection = DriverManager.getConnection(url,username,password)) {
+            System.out.println("====================================");
+            System.out.println("Test connection created "+connection);
+
+            //create
+            //String sql = "create table account (id int,username varchar(255),password varchar(255));";
+
+            //insert
+            String sql = "insert into account VALUES(1,'zuiop13','pass');";
+
+            try(PreparedStatement statement = connection.prepareStatement(sql)){
+                statement.execute();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
