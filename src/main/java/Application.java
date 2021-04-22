ca3ebf9fed0910bd6f7654bf68a569506b0fa1f3
PRO <zuiop13@naver.com>
2021-04-22 14:48:20 +0900

    새 pc 테스트 컴잇

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -17,7 +17,7 @@ public class Application {
             //create
             //String sql = "create table account (id int,username varchar(255),password varchar(255));";
 
-            //insert
+            //insert 현재 테스트 중입니다.
             String sql = "insert into account VALUES(1,'zuiop13','pass');";
 
             try(PreparedStatement statement = connection.prepareStatement(sql)){
